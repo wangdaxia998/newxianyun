411317aa6a3d4c2609b0373b4b929b084827c6ba
potato <13076335686@163.com>
2020-02-07 11:02:53 +0800

    项目初始化

---
+++ layouts/default.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <!-- 内容占位组件 -->
+    <myheader/>
+    <nuxt />
+  </div>
+</template>
+
+<script>
+import myheader from '@/components/header.vue'
+export default {
+  components:{
+    myheader
+  }
+}
+</script>
+
+<style lang="less">
+html {
+  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
+    Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+
+*{
+  margin:0; 
+  padding:0;
+}
+ul, li, ol{
+  list-style:none;
+}
+a{
+  text-decoration:none;
+  color:inherit;
+}
+a:hover{
+  
+}
+em,i{
+  font-style: normal;
+}
+</style>
