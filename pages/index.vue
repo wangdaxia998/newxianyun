88076b71c5ea2544178ccefb568bbad2ceb08eb8
potato <13076335686@163.com>
2020-02-07 19:27:08 +0800

    搜索栏(上)

--- pages/index.vue
+++ pages/index.vue
@@ -1,42 +1,174 @@
 <template>
   <div class="index">
+    <!-- 轮播图 -->
     <div class="block">
       <span class="demonstration"></span>
       <el-carousel height="700px">
-        <el-carousel-item v-for="(item,index) in list" :key="index" height="700px">
-          <div class="imgs" :style="`background:url(${$axios.defaults.baseURL}${item.url}) center center no-repeat `"></div>
+        <el-carousel-item v-for="(item,index) in list" :key="index" >
+          <div
+            class="imgs"
+            :style="`background:url(${$axios.defaults.baseURL}${item.url}) center center no-repeat `"
+          ></div>
         </el-carousel-item>
       </el-carousel>
+    </div>
+    <!-- 搜索框 -->
+    <div class="banner-content">
+      <div class="search-bar">
+        <!-- tab栏 -->
+        <el-row type="flex" class="search-tab">
+          <span v-for="(item,index) in tabss" :key="index" @click="dianji(index)" :class="{active:shuzi===index}">
+            <i>{{item.name}}</i>
+          </span>
+        </el-row>
+        <!-- 输入框 -->
+        <el-row type="flex" align="middle" class="search-input">
+          <input :placeholder="tabss[shuzi].placeholder"/>
+          <i class="el-icon-search" @click="handleSearch"></i>
+        </el-row>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  data(){
-    return{
-      list:[]
-    }
+  data() {
+    return {
+      list: [],
+      tabss:[
+        {
+          name:'攻略',
+          placeholder:'搜索城市',
+        },
+        {
+          name:'酒店',
+          placeholder:'请搜索城市搜索酒店'
+        },
+        {
+          name:'机票',
+          placeholder:''
+        }
+      ],
+      shuzi:0
+    };
   },
-  mounted(){
+  mounted() {
     this.$axios({
-      url:'/scenics/banners',
+      url: "/scenics/banners"
     }).then(res => {
       console.log(res);
-      this.list = res.data.data
-    })
+      this.list = res.data.data;
+    });
+  },
+  methods:{
+    handleSearch(){
+
+    },
+    dianji(index){
+      this.shuzi = index
+      if(index===2){
+        this.$router.push({ path:'/air'})
+      }
+    }
   }
 };
 </script>
 
 <style scoped lang='less'>
-.index{
+.index {
   min-width: 1000px;
-  .imgs{
-  width: 100%;
-  height: 100%;
-}
-}
+  .imgs {
+    width: 100%;
+    height: 100%;
+  }
+  .banner-content {
+    z-index: 9;
+    width: 1000px;
+    position: absolute;
+    left: 50%;
+    top: 45%;
+    margin-left: -500px;
+    border-top: 1px transparent solid;
 
+    .search-bar {
+      width: 552px;
+      margin: 0 auto;
+    }
 
+    .search-tab {
+      .active {
+        i {
+          color: #333;
+        }
+        &::after {
+          background: #eee;
+        }
+      }
+
+      span {
+        width: 82px;
+        height: 36px;
+        display: block;
+        position: relative;
+        margin-right: 8px;
+        cursor: pointer;
+
+        i {
+          position: absolute;
+          z-index: 2;
+          display: block;
+          width: 100%;
+          height: 100%;
+          line-height: 30px;
+          text-align: center;
+          color: #fff;
+        }
+
+        &:after {
+          position: absolute;
+          left: 0;
+          top: 0;
+          display: block;
+          content: "";
+          width: 100%;
+          height: 100%;
+          border: 1px rgba(255, 255, 255, 0.2) solid;
+          border-bottom: none;
+          transform: scale(1.1, 1.3) perspective(0.7em) rotateX(2.2deg);
+          transform-origin: bottom left;
+          background: rgba(0, 0, 0, 0.5);
+          border-radius: 1px 2px 0 0;
+          box-sizing: border-box;
+        }
+      }
+    }
+
+    .search-input {
+      width: 550px;
+      height: 46px;
+      background: #fff;
+      border-radius: 0 4px 4px 4px;
+      border: 1px rgba(255, 255, 255, 0.2) solid;
+      border-top: none;
+      box-sizing: unset;
+
+      input {
+        flex: 1;
+        height: 20px;
+        padding: 13px 15px;
+        outline: none;
+        border: 0;
+        font-size: 16px;
+      }
+
+      .el-icon-search {
+        cursor: pointer;
+        font-size: 22px;
+        padding: 0 10px;
+        font-weight: bold;
+      }
+    }
+  }
+}
 </style>
